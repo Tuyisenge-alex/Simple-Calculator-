d2b66e57dec63636b7ea884117de91bf4d6989b9
Tuyisenge-alex <tuyisengealex071@gmail.com>
2025-11-02 13:17:33 -0800

    first simple calculator project

---
+++ script.js
@@ -0,0 +1,21 @@
+function appendToDisplay(value) {
+  document.getElementById("result").value += value;
+}
+
+function clearDisplay() {
+  document.getElementById("result").value = "";
+}
+
+function deleteLast() {
+  let current = document.getElementById("result").value;
+  document.getElementById("result").value = current.slice(0, -1);
+}
+
+function calculateResult() {
+  try {
+    let result = eval(document.getElementById("result").value);
+    document.getElementById("result").value = result;
+  } catch (error) {
+    document.getElementById("result").value = "Error";
+  }
+}
